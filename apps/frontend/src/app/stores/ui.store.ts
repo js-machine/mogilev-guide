b7dcb046b2099f21d85bb333114b971bd066f87e
Dzianis Pasiukou <Dzianis_Pasiukou1@epam.com>
2020-01-24 10:16:42 +0300

    fix: lint

--- apps/frontend/src/app/stores/ui.store.ts
+++ apps/frontend/src/app/stores/ui.store.ts
@@ -3,9 +3,6 @@ import { action, observable } from 'mobx';
 export class UiStore {
   @observable public isPageLoading?: boolean;
 
-  public constructor() {
-  }
-
   @action public setIsLoading = (isPageLoading: boolean) => {
     this.isPageLoading = isPageLoading;
   };
