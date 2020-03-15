49b21350aab2efe109ca085a0e59bf2ea2743112
Aliaksei_Zhabitsenka <Aliaksei_Zhabitsenka@epam.com>
2020-03-15 15:32:26 +0300

    fix: sight mock view all photos/reviews

--- apps/frontend/src/app/scenes/sight/sight.tsx
+++ apps/frontend/src/app/scenes/sight/sight.tsx
@@ -22,56 +22,11 @@ export const SightRaw: React.FC<Props> = props => {
     }, [id, sightStore]);
 
     const handleViewAllPhotos = useCallback(() => {
-        // TODO: Mock
-        runInAction(() => {
-            sightStore.sight = ({
-                ...sightStore.sight,
-                photos:[
-                    ...sightStore.sight.photos,
-                    'https://planetabelarus.by/upload/resize_cache/iblock/95a/1330_887_18e21fe612b4afb807a26ecc22279a1d9/95af5cc42e396e28a0e909fae9e75ca0.jpg',
-                ],
-            });
-        });
+        console.log('view all photos');
     }, [sightStore.sight]);
 
     const handleViewAllReviews = useCallback(() => {
-        // TODO: Mock
-        runInAction(() => {
-            sightStore.sight = ({
-                ...sightStore.sight,
-                reviews: [
-                    ...sightStore.sight.reviews,
-                    {
-                        id: 'review228',
-                        user: {
-                            id: 'user228',
-                            login: 'foo',
-                            email: 'foo',
-                            firstName: 'Foo',
-                            lastName: 'Bar',
-                            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSxbJ-KNiELvIR_0q42fjBldG89LLI6869wq_hmVVmccZN3BDn4',
-                        },
-                        date: Date.now(),
-                        rating: 4,
-                        message: 'Some message here',
-                    },
-                    {
-                        id: 'review322',
-                        user: {
-                            id: 'user322',
-                            login: 'bar',
-                            email: 'bar',
-                            firstName: 'Bar',
-                            lastName: 'Foo',
-                            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSxbJ-KNiELvIR_0q42fjBldG89LLI6869wq_hmVVmccZN3BDn4',
-                        },
-                        date: Date.now(),
-                        rating: 3,
-                        message: 'Dolor sit amet consectetur. Dolor sit amet consectetur. Dolor sit amet consectetur. Dolor sit amet consectetur. Dolor sit amet consectetur. Dolor sit amet consectetur. Dolor sit amet consectetur.',
-                    }
-                ]
-            });
-        });
+        console.log('view all reviews')
     }, [sightStore.sight]);
 
     if (uiStore.isPageLoading) {
